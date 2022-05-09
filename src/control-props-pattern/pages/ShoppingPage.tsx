6a724aa096fa067acd7307491bf69a87ac1faf3b
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-09 15:56:47 -0300

    ProductImage Component

--- src/control-props-pattern/pages/ShoppingPage.tsx
+++ src/control-props-pattern/pages/ShoppingPage.tsx
@@ -1,4 +1,5 @@
 import ProductCard from '../components/ProductCard';
+import { ProductImg } from '../components/ProductImage';
 
 import "../styles/custom-styles.css";
 const product = {
@@ -13,6 +14,9 @@ const ShoppingPage = () => {
       <hr />
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
       <ProductCard product={product} className="bg-dark text-white">
+      <ProductImg
+       className="custom-image"
+       style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)"}} />
       </ProductCard>        
       </div>
     </div>
