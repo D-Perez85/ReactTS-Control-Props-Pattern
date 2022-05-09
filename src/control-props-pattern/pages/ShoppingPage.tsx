4f1560b0e4b1a6b6a13dbc338122e8e597dba0d0
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-09 15:56:03 -0300

    ProductCard Component

--- src/control-props-pattern/pages/ShoppingPage.tsx
+++ src/control-props-pattern/pages/ShoppingPage.tsx
@@ -1,12 +1,19 @@
-import "../styles/custom-styles.css";
+import ProductCard from '../components/ProductCard';
 
+import "../styles/custom-styles.css";
+const product = {
+  id: "1",
+  title: "Delicious Cafe",
+  img: "./coffee-mug.png",
+};
 const ShoppingPage = () => {
   return (
     <div>
       <h1> Shopping Store </h1>
       <hr />
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-        
+      <ProductCard product={product} className="bg-dark text-white">
+      </ProductCard>        
       </div>
     </div>
   );
