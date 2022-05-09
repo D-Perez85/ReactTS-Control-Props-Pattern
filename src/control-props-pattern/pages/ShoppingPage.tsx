e35273330b2d9ddfb01d0dae7e310f7c97f14c7f
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-09 15:59:58 -0300

    ProductButtons Component

--- src/control-props-pattern/pages/ShoppingPage.tsx
+++ src/control-props-pattern/pages/ShoppingPage.tsx
@@ -1,3 +1,4 @@
+import { ProductButtons } from '../components/ProductButtons';
 import ProductCard from '../components/ProductCard';
 import { ProductImg } from '../components/ProductImage';
 
@@ -14,9 +15,10 @@ const ShoppingPage = () => {
       <hr />
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
       <ProductCard product={product} className="bg-dark text-white">
-      <ProductImg
-       className="custom-image"
-       style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)"}} />
+        <ProductImg
+            className="custom-image"
+            style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)"}} />
+        <ProductButtons className="custom-buttons" />
       </ProductCard>        
       </div>
     </div>
