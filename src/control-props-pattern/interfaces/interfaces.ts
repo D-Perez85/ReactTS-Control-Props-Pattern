47043a14efce12a5b268049acc810345124d710e
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-09 16:42:42 -0300

    Initial State of Shopping Cart

--- src/control-props-pattern/interfaces/interfaces.ts
+++ src/control-props-pattern/interfaces/interfaces.ts
@@ -1,12 +1,15 @@
-  export interface Product {
-    id: string;
-    img?: string;
-    title: string;
-  }
-  
-  export interface ProductContextProps {
-    product: Product, 
-    counter: number,
-    increaseBy:(value:number)=> void,
-  }
+export interface Product {
+  id: string;
+  img?: string;
+  title: string;
+}
 
+export interface ProductContextProps {
+  product: Product;
+  counter: number;
+  increaseBy: (value: number) => void;
+}
+//EXTIENDO DE PRODUCT PORQUE SOLO SE AGREGA UN CAMPO
+export interface ProductInCart extends Product {
+  count: number;
+}
--- src/control-props-pattern/pages/ShoppingPage.tsx
+++ src/control-props-pattern/pages/ShoppingPage.tsx
@@ -1,40 +1,50 @@
-import { ProductButtons } from '../components/ProductButtons';
-import ProductCard from '../components/ProductCard';
-import { ProductImg } from '../components/ProductImage';
-import { ProductTitle } from '../components/ProductTitle';
+import { useState } from "react";
+import ProductCard from "../components/ProductCard";
+import { ProductImg } from "../components/ProductImage";
+import { ProductButtons } from "../components/ProductButtons";
+import { ProductTitle } from "../components/ProductTitle";
+import { products } from "../data/products";
+import { ProductInCart } from "../interfaces/interfaces";
 
 import "../styles/custom-styles.css";
-import { products } from "../data/products";
 
 const ShoppingPage = () => {
+  const [shoppingCart, setShoppingCart] = useState<{[key: string]: ProductInCart}>({
+    // '1':{...products[0], count: 10},
+    // '2':{...products[1], count: 5}
+  });
+console.log(shoppingCart);
+
   return (
     <div>
       <h1> Shopping Store </h1>
       <hr />
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-      {products.map((product) => (
-
-      <ProductCard product={product} className="bg-dark text-white">
-        <ProductImg
-            className="custom-image"
-            style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)"}} />
-        <ProductTitle/>
-        <ProductButtons className="custom-buttons" />
-      </ProductCard>        
-      ))}
+        {products.map((product) => (
+          <ProductCard
+            product={product}
+            className="bg-dark text-white">
+            <ProductImg
+              className="custom-image"
+              style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}/>
+            <ProductTitle />
+            <ProductButtons className="custom-buttons" />
+          </ProductCard>
+        ))}
       </div>
 
       <div className="shopping-cart">
-          <ProductCard
-            product={products[1]}
-            className="bg-dark text-white"
-            style={{ width: "100px" }}>
-              <ProductImg className="custom-image" style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}/>
-              <ProductButtons 
-              className="custom-buttons" 
-              style={{ display: "flex", justifyContent: "center" }}
-              />
-          </ProductCard>
+        <ProductCard
+          product={products[1]}
+          className="bg-dark text-white"
+          style={{ width: "100px" }}>
+          <ProductImg
+            className="custom-image"
+            style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}/>
+          <ProductButtons
+            className="custom-buttons"
+            style={{ display: "flex", justifyContent: "center" }}/>
+        </ProductCard>
       </div>
     </div>
   );
